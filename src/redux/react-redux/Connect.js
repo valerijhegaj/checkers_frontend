9c0b92b39e37abbe33392da9d06d7cd53cc2007e
valerijhegaj <valerijhegaj@gmail.com>
2022-10-20 16:14:36 +0300

    refactor connect to class

--- src/redux/react-redux/Connect.js
+++ src/redux/react-redux/Connect.js
@@ -1,6 +1,12 @@
 import MyContext from "./Provider";
+import React from "react"
 
-const Connect = (mapStateToProps = () => {}, mapDispatchToProps = () => {}) => (component) => {
+const defaultMap = () => {
+}
+
+const ConnectWithoutSubscribe = (mapStateToProps = defaultMap,
+                                 mapDispatchToProps = defaultMap
+) => (component) => {
   const componentWithProps = (props) => (store) => {
     const stateProps = mapStateToProps(store.GetState())
     const dispatchProps = mapDispatchToProps(store.Dispatch.bind(store))
@@ -11,6 +17,48 @@ const Connect = (mapStateToProps = () => {}, mapDispatchToProps = () => {}) => (
   return (props) => {
     return <MyContext.Consumer>
       {componentWithProps(props)}
+    </MyContext.Consumer>
+  }
+}
+
+const Connect = (mapStateToProps = defaultMap,
+                 mapDispatchToProps = defaultMap) => (Component) => {
+  class Container_ extends React.Component {
+    constructor(props) {
+      super(props)
+
+      const store = this.props.store
+      this._dispatch = store.Dispatch.bind(store)
+    }
+
+    _getState() {
+      return this.props.store.GetState()
+    }
+
+    render() {
+      const stateProps = mapStateToProps(this._getState())
+      const dispatchProps = mapDispatchToProps(this._dispatch)
+      return (
+        <Component {...this.props} {...stateProps} {...dispatchProps}/>
+      )
+    }
+
+    componentDidMount() {
+      this.unsubscribe = this.props.store.Subscribe(this._handleChange.bind(this))
+    }
+
+    componentWillUnmount() {
+      this.unsubscribe()
+    }
+
+    _handleChange() {
+      this.forceUpdate()
+    }
+  }
+
+  return props => {
+    return <MyContext.Consumer>
+      {store => <Container_ {...props} store={store}/>}
     </MyContext.Consumer>
   }
 }
